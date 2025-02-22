871e162d0f75ac3ecd36bf7be426cfe41231512f
alhogiu <144394068+alhogiu@users.noreply.github.com>
2025-02-22 14:11:31 -0500

    added unit and e2e test for recipe component

---
+++ client/src/tests/recipe.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import RecipeView from '../components/Recipe/RecipeView';
+import { MemoryRouter } from 'react-router-dom';  // Import MemoryRouter
+import '@testing-library/jest-dom'
+
+describe('RecipeLoading', () => {
+    it('loads recipe chosen on first load', () => {
+        const getRecipe = jest.fn().mockName('getRecipe');
+        const recipe= {};
+        const ingredients = [];
+        render(
+            <MemoryRouter>
+                <RecipeView getRecipe={getRecipe} recipe={recipe} ingredients={ingredients} />
+            </MemoryRouter>
+        );
+        expect(getRecipe).toHaveBeenCalled();
+    });
+    it('uses recipe name', () => {
+        const getRecipe = jest.fn().mockName('getRecipe');
+        const recipe= {name: "test recipe"};
+        const ingredients = [];
+        render(
+            <MemoryRouter>
+                <RecipeView getRecipe={getRecipe} recipe={recipe} ingredients={ingredients} />
+            </MemoryRouter>
+        );
+        expect(screen.getByText('test recipe')).toBeInTheDocument();
+    });
+});
